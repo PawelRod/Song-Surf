4217983e4099feb53f39e384c84cf0cbd62a416f
PawelRod <rodziewiczpp@gmail.com>
2020-03-15 17:37:00 +0100

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
